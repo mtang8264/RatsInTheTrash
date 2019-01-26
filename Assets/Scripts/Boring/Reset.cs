716479851cdc030730ca599abe6605776873cd0c
Michael Tang <mtang8264@gmail.com>
2019-01-26 17:54:19 -0500

    Geneva

---
+++ Assets/Scripts/Boring/Reset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    public int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            count++;
+        }
+
+        if(count > 20)
+        {
+            File.Delete(Application.persistentDataPath + "/rats.rts");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
