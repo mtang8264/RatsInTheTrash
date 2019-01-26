aa71db6095a1fa79baaf769f2dceaa18e1dcff66
Michael Tang <mtang8264@gmail.com>
2019-01-26 18:29:06 -0500

    Basic works

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Circle : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class Circle : MonoBehaviour
     public int currentQuad = -1;
     public List<int> quadrants = new List<int>();
 
+    public int rotation = 4;
+
     public Transform reel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        quadrants.RemoveRange(0,quadrants.Count);
     }
 
     // Update is called once per frame
@@ -42,6 +45,63 @@ public class Circle : MonoBehaviour
             }
         }
 
+        if (quadrants.Count > rotation * 4)
+        {
+            quadrants.RemoveAt(0);
+        }
 
+        if (lastQuad != currentQuad)
+        {
+            quadrants.Add(currentQuad);
+            lastQuad = currentQuad;
+
+        }
+
+        bool captured = true;
+        bool left = false;
+        int lookingFor = quadrants[0];
+        int greater = lookingFor + 1;
+        greater = greater > 4 ? 1 : greater;
+        int lesser = lookingFor - 1;
+        lesser = lesser < 1 ? 4 : lesser;
+        if(quadrants[1] == greater)
+        {
+            left = true;
+        }
+        else if(quadrants[1] == lesser)
+        {
+            left = false;
+        }
+
+        if(left)
+        {
+            for (int i = 0; i < quadrants.Count; i ++)
+            {
+                if(quadrants[i] != lookingFor)
+                {
+                    captured = false;
+                }
+                lookingFor++;
+                lookingFor = lookingFor > 4 ? 1 : lookingFor;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < quadrants.Count; i ++)
+            {
+                if(quadrants[i] != lookingFor)
+                {
+                    captured = false;
+                }
+                lookingFor--;
+                lookingFor = lookingFor < 1 ? 4 : lookingFor;
+            }
+        }
+
+        if(captured && quadrants.Count > rotation * 4 -2)
+        {
+            DecorationHandler.toPlace = GetComponent<FishingSpot>().floater;
+            SceneManager.LoadScene(0);
+        }
     }
 }
--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestrohy : MonoBehaviour
+public class Reel : MonoBehaviour
 {
-    public static bool d = false;
     // Start is called before the first frame update
     void Start()
     {
-        if (d)
-            Destroy(gameObject);
-        d = true;
-        DontDestroyOnLoad(gameObject);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
