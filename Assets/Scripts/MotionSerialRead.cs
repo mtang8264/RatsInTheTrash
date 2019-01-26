eb3f1d20b504d98add31ad39f9dd705773ef48d6
Michael Tang <mtang8264@gmail.com>
2019-01-25 21:35:12 -0500

    Arduino serial stuff

---
+++ Assets/Scripts/MotionSerialRead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MotionSerialRead : MonoBehaviour
+{
+    //SerialPort serial = new SerialPort("COM4", 9600);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime,0, Input.GetAxis("Vertical") * Time.deltaTime);
+        movement *= speed;
+        transform.Translate(transform.InverseTransformVector(movement));
+    }
+}
